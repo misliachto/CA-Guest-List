465175fbc1dfeb5f07aa111e2867f252dbccf10e
Mindaugas <Mindaugas.sliachtovicius@gmail.com>
2021-08-16 21:22:53 +0300

    Add few new functions
    Added Reverse list and fixed first element removal

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btnAddFirst = document.querySelector("#btn-add-first");
 const btnAddLast = document.querySelector("#btn-add-last");
 const btnRemoveFirst = document.querySelector("#btn-remove-first");
 const btnRemoveLast = document.querySelector("#btn-remove-last");
+const btnReverseList = document.querySelector("#btn-reverse-list");
 
 
 const nameInput = document.querySelector('#name-input');
@@ -10,33 +11,38 @@ const inviteeList = document.querySelector('.invitees-list');
 let inviteeArray = [];
 
 // FUNCTIONS
-function getNameInput() {//??
+const getNameInput = () => {//??
     return parseFloat(nameInput.value)
 }
 //  --adding guest to the end of the list
-function addFirstGuest() {
+const addFirstGuest = () => {
     inviteeArray.unshift(`<li>${nameInput.value}</li>`);
     outputInviteeList();
 }
-
 //  --adding guest to the end of the list
-function addLastGuest() {
+const addLastGuest = () => {
     inviteeArray.push(`<li>${nameInput.value}</li>`);
     outputInviteeList();
 }
 //  --removing last name from array
-function removeLastGuest() {
-    inviteeArray = inviteeArray.slice(0,-1)
+const removeLastGuest = () => {
+    inviteeArray = inviteeArray.slice(0, -1)
     outputInviteeList();
     return
 }
 //  --removing first name from arra
-function removeFirstGuest() {
-    inviteeArray = inviteeArray.shift();
+const removeFirstGuest = () => {
+    inviteeArray = inviteeArray.slice(1);
     outputInviteeList();
     return
 }
-
+//  --reversing list
+const reverseList = () => {
+    let reversed = inviteeArray.reverse();
+    inviteeArray = reversed;
+    outputInviteeList();
+    return
+}
 // --outputting content
 function outputInviteeList() {
     inviteeList.innerHTML = inviteeArray.toString();
@@ -48,15 +54,11 @@ btnAddFirst.addEventListener("click", addFirstGuest);
 btnAddLast.addEventListener("click", addLastGuest);
 btnRemoveLast.addEventListener("click", removeLastGuest);
 btnRemoveFirst.addEventListener("click", removeFirstGuest);
+btnReverseList.addEventListener("click", reverseList);
 
 
 
 /*
-- “Remove First” - to remove first name from the list
-- “Remove Last” - to remove last name from the list
-- “Reverse List” - reverse this list
-Of course output the list in the screen for user to see.
-Bonus: if you are faster, you can try to add these as well:
 - “Remove from - to” - to remove list items from certain index to certain point in the list.
 If only from provided - delete only 1 item. Will need additional input fields.
 - “Add at certain point” - to add name at certain point in the list.
